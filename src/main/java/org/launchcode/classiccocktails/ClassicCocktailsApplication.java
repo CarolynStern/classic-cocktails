c3fce6b6962366683e051b4e1e6f2cec6e282b66
Carolyn Stern <carolynmstern@gmail.com>
2019-01-30 20:09:32 -0600

    deleted files

---
+++ src/main/java/org/launchcode/classiccocktails/ClassicCocktailsApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.classiccocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicCocktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicCocktailsApplication.class, args);
+	}
+
+}
+
